4b46b4d59e66657b14a746d1594a5c2954d7cfa5
Alberto <betim009@gmail.com>
2024-04-22 14:58:34 -0300

    Primeira aula

---
+++ Course/1.TiposDeDados/script2.js
@@ -0,0 +1,7 @@
+import { dataTeams } from "./script.js";
+
+// Acessando o primeiro elemento do array dataTeams
+console.log(dataTeams[0]);
+
+// Acessando o último elemento do array dataTeams
+console.log(dataTeams[dataTeams.length - 1]);  // 30 - 1 -> 29
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Course_Javascript
+# JavaScript
+
+* [ ]  1.Tipos de dados
+* [ ]  2.Condições
+* [ ]  3.Repetições
+* [ ]  4.Funções
+* [ ]  5.JsDom
