8af43305699bde9caa4dd1bb85c975d9701f1ad8
Henrique Versiani <henriversiani@gmail.com>
2024-05-03 14:02:51 -0300

    data times western

--- Projetos/data.js
+++ Projetos/data.js
@@ -1,12 +1,124 @@
 const dataWestern = [
     {
         "id": 1,
-        "team": "time 1",
+        "team": "Los Angeles Lakers",
         "conference": "Western",
-        "win": 0,
-        "loss": 0,
-        "imgLogo": "SEM PRIORIDADE"
+        "win": 47,
+        "loss": 35,
+        "imgLogo": ""
     },
+    {
+        "id": 2,
+        "team": "Los Angeles Clippers",
+        "conference": "Western",
+        "win": 51,
+        "loss": 31,
+        "imgLogo": ""
+    },
+    {
+        "id": 3,
+        "team": "Golden State Warriors",
+        "conference": "Western",
+        "win": 46,
+        "loss": 36,
+        "imgLogo": ""
+    },
+    {
+        "id": 4,
+        "team": "Phoenix Suns",
+        "conference": "Western",
+        "win": 49,
+        "loss": 33,
+        "imgLogo": ""
+    },
+    {
+        "id": 5,
+        "team": "Denver Nuggets",
+        "conference": "Western",
+        "win": 57,
+        "loss": 25,
+        "imgLogo": ""
+    },
+    {
+        "id": 6,
+        "team": "Utah Jazz",
+        "conference": "Western",
+        "win": 31,
+        "loss": 51,
+        "imgLogo": ""
+    },
+    {
+        "id": 7,
+        "team": "Portland Trail Blazers",
+        "conference": "Western",
+        "win": 21,
+        "loss": 61,
+        "imgLogo": ""
+    },
+    {
+        "id": 8,
+        "team": "Dallas Mavericks",
+        "conference": "Western",
+        "win": 50,
+        "loss": 32,
+        "imgLogo": ""
+    },
+    {
+        "id": 9,
+        "team": "Memphis Grizzlies",
+        "conference": "Western",
+        "win": 27,
+        "loss": 55,
+        "imgLogo": ""
+    },
+    {
+        "id": 10,
+        "team": "San Antonio Spurs",
+        "conference": "Western",
+        "win": 22,
+        "loss": 60,
+        "imgLogo": ""
+    },
+    {
+        "id": 11,
+        "team": "New Orleans Pelicans",
+        "conference": "Western",
+        "win": 49,
+        "loss": 33,
+        "imgLogo": ""
+    },
+    {
+        "id": 12,
+        "team": "Minnesota Timberwolves",
+        "conference": "Western",
+        "win": 56,
+        "loss": 26,
+        "imgLogo": ""
+    },
+    {
+        "id": 13,
+        "team": "Sacramento Kings",
+        "conference": "Western",
+        "win": 46,
+        "loss": 36,
+        "imgLogo": ""
+    },
+    {
+        "id": 14,
+        "team": "Houston Rockets",
+        "conference": "Western",
+        "win": 41,
+        "loss": 41,
+        "imgLogo": ""
+    },
+    {
+        "id": 15,
+        "team": "Oklahoma City Thunder",
+        "conference": "Western",
+        "win": 57,
+        "loss": 25,
+        "imgLogo": ""
+    }
 ];
 
 const dataEastern = [
