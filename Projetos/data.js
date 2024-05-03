a72d2b072cd9aecd0695d16c913e8f3b332ad541
Henrique Versiani <henriversiani@gmail.com>
2024-05-03 14:13:55 -0300

    imagens conferencia western

--- Projetos/data.js
+++ Projetos/data.js
@@ -5,7 +5,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 47,
         "loss": 35,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/4ndR-n-gall7_h3f7NYcpQ_48x48.png"
     },
     {
         "id": 2,
@@ -13,7 +13,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 51,
         "loss": 31,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/F36nQLCQ2FND3za-Eteeqg_48x48.png"
     },
     {
         "id": 3,
@@ -21,7 +21,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 46,
         "loss": 36,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/ovwlyYHRKZ90s7zn_qlMCg_48x48.png"
     },
     {
         "id": 4,
@@ -29,7 +29,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 49,
         "loss": 33,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/pRr87i24KHWH0UuAc5EamQ_48x48.png"
     },
     {
         "id": 5,
@@ -37,7 +37,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 57,
         "loss": 25,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/9wPFTOxV_zP1KmRRggJNqQ_48x48.png"
     },
     {
         "id": 6,
@@ -45,7 +45,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 31,
         "loss": 51,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/xxdtYBIrtmG-BKEdbxpTBw_48x48.png"
     },
     {
         "id": 7,
@@ -53,7 +53,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 21,
         "loss": 61,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/_bgagBCd6ieOIt3INWRN_w_48x48.png"
     },
     {
         "id": 8,
@@ -61,7 +61,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 50,
         "loss": 32,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/xxxlj9RpmAKJ9P9phstWCQ_48x48.png"
     },
     {
         "id": 9,
@@ -69,7 +69,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 27,
         "loss": 55,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/3ho45P8yNw-WmQ2m4A4TIA_48x48.png"
     },
     {
         "id": 10,
@@ -77,7 +77,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 22,
         "loss": 60,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/FKwMB_85FlZ_7PTt1f7hjQ_48x48.png"
     },
     {
         "id": 11,
@@ -85,7 +85,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 49,
         "loss": 33,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/JCQO978-AWbg00TQUNPUVg_48x48.png"
     },
     {
         "id": 12,
@@ -93,7 +93,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 56,
         "loss": 26,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/21Zm6e_zGiWXsaLCQyjVig_48x48.png"
     },
     {
         "id": 13,
@@ -101,7 +101,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 46,
         "loss": 36,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/wkCDHakxEThLGoZ4Ven48Q_48x48.png"
     },
     {
         "id": 14,
@@ -109,7 +109,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 41,
         "loss": 41,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/zhO6MIB1UzZmtXLHkJQBmg_48x48.png"
     },
     {
         "id": 15,
@@ -117,7 +117,7 @@ const dataWestern = [
         "conference": "Western",
         "win": 57,
         "loss": 25,
-        "imgLogo": ""
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/b4bJ9zKFBDykdSIGUrbWdw_48x48.png"
     }
 ];
 
