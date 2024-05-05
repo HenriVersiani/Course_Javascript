ee91f16fd22a38637f654ccfd0b3a261f344d011
Henrique Versiani <henriversiani@gmail.com>
2024-05-05 10:33:23 -0300

    Preenchendo eastern e btn-western

--- Projetos/data.js
+++ Projetos/data.js
@@ -126,8 +126,120 @@ export const dataEastern = [
         "id": 16,
         "team": "Celtics",
         "conference": "Eastern",
-        "win": 0,
-        "loss": 0,
+        "win": 64,
+        "loss": 18,
         "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/GDJBo7eEF8EO5-kDHVpdqw_48x48.png"
     },
+    {
+        "id": 17,
+        "team": "Bucks",
+        "conference": "Eastern",
+        "win": 49,
+        "loss": 33,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/Wd6xIEIXpfqg9EZC6PAepQ_48x48.png"
+    },
+    {
+        "id": 18,
+        "team": "76ers",
+        "conference": "Eastern",
+        "win": 47,
+        "loss": 35,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/US6KILZue2D5766trEf0Mg_48x48.png"
+    },
+    {
+        "id": 19,
+        "team": "Nets",
+        "conference": "Eastern",
+        "win": 32,
+        "loss": 50,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/iishUmO7vbJBE7iK2CZCdw_48x48.png"
+    },
+    {
+        "id": 20,
+        "team": "Heat",
+        "conference": "Eastern",
+        "win": 46,
+        "loss": 36,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/0nQXN6OF7wnLY3hJz8lZJQ_48x48.png"
+    },
+    {
+        "id": 21,
+        "team": "Knicks",
+        "conference": "Eastern",
+        "win": 50,
+        "loss": 32,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/-rf7eY39l_0V7J4ekakuKA_48x48.png"
+    },
+    {
+        "id": 22,
+        "team": "Hawks",
+        "conference": "Eastern",
+        "win": 36,
+        "loss": 46,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/pm5l5mtY1elOQAl9ZEcm2A_48x48.png"
+    },
+    {
+        "id": 23,
+        "team": "Hornets",
+        "conference": "Eastern",
+        "win": 21,
+        "loss": 61,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/ToeKy5-TrHAnTCl-qhuuHQ_48x48.png"
+    },
+    {
+        "id": 24,
+        "team": "Bulls",
+        "conference": "Eastern",
+        "win": 39,
+        "loss": 43,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/ofjScRGiytT__Flak2j4dg_48x48.png"
+    },
+    {
+        "id": 25,
+        "team": "Cavaliers",
+        "conference": "Eastern",
+        "win": 48,
+        "loss": 34,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/NAlGkmv45l1L-3NhwVhDPg_48x48.png"
+    },
+    {
+        "id": 26,
+        "team": "Pistons",
+        "conference": "Eastern",
+        "win": 14,
+        "loss": 68,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/qvWE2FgBX0MCqFfciFBDiw_48x48.png"
+    },
+    {
+        "id": 27,
+        "team": "Pacers",
+        "conference": "Eastern",
+        "win": 47,
+        "loss": 35,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/andumiE_wrpDpXvUgqCGYQ_48x48.png"
+    },
+    {
+        "id": 28,
+        "team": "Magic",
+        "conference": "Eastern",
+        "win": 47,
+        "loss": 35,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/p69oiJ4LDsvCJUDQ3wR9PQ_48x48.png"
+    },
+    {
+        "id": 29,
+        "team": "Raptors",
+        "conference": "Eastern",
+        "win": 27,
+        "loss": 57,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/745IgW4NSvnRxg-W9oczmQ_48x48.png"
+    },
+    {
+        "id": 30,
+        "team": "Wizards",
+        "conference": "Eastern",
+        "win": 15,
+        "loss": 67,
+        "imgLogo": "https://ssl.gstatic.com/onebox/media/sports/logos/NBkMJapxft4V5kvufec4Jg_48x48.png"
+    }
 ];
--- Projetos/script.js
+++ Projetos/script.js
@@ -1,40 +1,63 @@
 import { dataEastern, dataWestern } from "./data.js";
 
 // Seletores
-const tbodyWestern = document.getElementById('tbody-western');
+const tbodyWestern = document.getElementById('tbody-conference');
+const tbodyEastern = document.getElementById('tbody-conference');
+
 const btnEastern = document.getElementById('btn-eastern');
+const btnWestern = document.getElementById('btn-western')
 
 // Variaveis
-const rankingDataWestern = dataWestern.sort((x, y) => y.win - x.win);
-console.log(rankingDataWestern);
+dataWestern.sort((x, y) => y.win - x.win);
+dataEastern.sort((x, y) => y.win - x.win);
 
+function loadWestern(){
 
-dataWestern.forEach((element, index) => {
-    tbodyWestern.innerHTML += `
-    <tr>
-        <th scope="row">${index + 1}</th>
-        <td> <img src="${element.imgLogo}" class="mx-2">${element.team}</td>
-        <td class="text-center">${element.win + element.loss}</td>
-        <td class="text-center">${element.win}</td>
-        <td class="text-center">${element.loss}</td>
-    </tr>
-    `;
-});
-
-// EVENTO de CLICK
-btnEastern.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    dataEastern.forEach((element, index) => {
-        tbodyWestern.innerHTML = "";
+    dataWestern.forEach((element, index) => {
         tbodyWestern.innerHTML += `
         <tr>
             <th scope="row">${index + 1}</th>
-            <td>${element.team}</td>
+            <td> <img src="${element.imgLogo}" class="mx-2">${element.team}</td>
             <td class="text-center">${element.win + element.loss}</td>
             <td class="text-center">${element.win}</td>
             <td class="text-center">${element.loss}</td>
         </tr>
         `;
-    });
+    })
+    return(tbodyWestern.innerHTML)
+}
+
+function loadEastern(){
+
+    dataEastern.forEach((element, index) => {
+        tbodyEastern.innerHTML += `
+        <tr>
+            <th scope="row">${index + 1}</th>
+            <td> <img src="${element.imgLogo}" class="mx-2">${element.team}</td>
+            <td class="text-center">${element.win + element.loss}</td>
+            <td class="text-center">${element.win}</td>
+            <td class="text-center">${element.loss}</td>
+        </tr>
+        `;
+    })  
+    return(tbodyEastern.innerHTML)
+}
+
+loadWestern()
+
+
+btnWestern.addEventListener('click',(event) =>{
+
+    event.preventDefault();
+    tbodyEastern.innerHTML = ''
+    loadWestern()
+});
+
+// EVENTO de CLICK
+btnEastern.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    tbodyWestern.innerHTML = ''
+    loadEastern()
+
 });
