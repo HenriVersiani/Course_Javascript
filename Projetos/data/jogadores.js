749458b533148f8243940b38f8903ed361092668
Leonardo Versiani Paiva <versi@NOTE-758.firstdecision.local>
2024-05-09 17:18:21 -0300

    jogadores western

--- Projetos/data/jogadores.js
+++ Projetos/data/jogadores.js
@@ -1,34 +1,343 @@
 const jogadoresWestern = [
     {
-        id: 1,
-        time: "Boston Celtics",
-        jogadores: [
+        "id": 1,
+        "time": "Los Angeles Lakers",
+        "jogadores": [
             {
-                nome: "Jason Tatun",
-                img: "",
-                posicao: ""
+                "nome": "LeBron James",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
             },
             {
-                nome: "Jason Tatun",
-                img: "",
-                posicao: ""
+                "nome": "Anthony Davis",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Russell Westbrook",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Carmelo Anthony",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Malcolm Brogdon",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
             }
         ]
     },
     {
-        id: 2,
-        time: "New York Knicks",
-        jogadores: [
+        "id": 2,
+        "time": "Golden State Warriors",
+        "jogadores": [
             {
-                nome: "Jaylen Brownson",
-                img: "",
-                posicao: ""
+                "nome": "Stephen Curry",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
             },
             {
-                nome: "Jason Tatun",
-                img: "",
-                posicao: ""
+                "nome": "Klay Thompson",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Draymond Green",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Andrew Wiggins",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Kevon Looney",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
             }
         ]
     },
+    {
+        "id": 3,
+        "time": "Denver Nuggets",
+        "jogadores": [
+            {
+                "nome": "Nikola Jokic",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Jamal Murray",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Aaron Gordon",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Michael Porter Jr.",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Monte Morris",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "time": "Utah Jazz",
+        "jogadores": [
+            {
+                "nome": "Donovan Mitchell",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Rudy Gobert",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Mike Conley",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Bojan Bogdanovic",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Jordan Clarkson",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "time": "Phoenix Suns",
+        "jogadores": [
+            {
+                "nome": "Devin Booker",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Chris Paul",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Deandre Ayton",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Mikal Bridges",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Jae Crowder",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            }
+        ]
+    },
+    {
+        "id": 6,
+        "time": "Portland Trail Blazers",
+        "jogadores": [
+            {
+                "nome": "Damian Lillard",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "CJ McCollum",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Norman Powell",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Robert Covington",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Jusuf Nurkic",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "time": "Dallas Mavericks",
+        "jogadores": [
+            {
+                "nome": "Luka Doncic",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Kristaps Porzingis",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Tim Hardaway Jr.",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Dorian Finney-Smith",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Jalen Brunson",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            }
+        ]
+    },
+    {
+        "id": 8,
+        "time": "Memphis Grizzlies",
+        "jogadores": [
+            {
+                "nome": "Ja Morant",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Jaren Jackson Jr.",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Dillon Brooks",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Steven Adams",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Desmond Bane",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "time": "New Orleans Pelicans",
+        "jogadores": [
+            {
+                "nome": "Zion Williamson",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Brandon Ingram",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Jonas Valanciunas",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Devonte' Graham",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Josh Hart",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            }
+        ]
+    },
+    {
+        "id": 10,
+        "time": "Minnesota Timberwolves",
+        "jogadores": [
+            {
+                "nome": "Karl-Anthony Towns",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            },
+            {
+                "nome": "Anthony Edwards",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "D'Angelo Russell",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Malik Beasley",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Jarrett Culver",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            }
+        ]
+    },
+    {
+        "id": 11,
+        "time": "Sacramento Kings",
+        "jogadores": [
+            {
+                "nome": "De'Aaron Fox",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Tyrese Haliburton",
+                "img": "url_da_imagem",
+                "posicao": "Armador"
+            },
+            {
+                "nome": "Harrison Barnes",
+                "img": "url_da_imagem",
+                "posicao": "Ala"
+            },
+            {
+                "nome": "Domantas Sabonis",
+                "img": "url_da_imagem",
+                "posicao": "Ala Pivo"
+            },
+            {
+                "nome": "Richaun Holmes",
+                "img": "url_da_imagem",
+                "posicao": "Pivo"
+            }
+        ]
+    }     
 ];
