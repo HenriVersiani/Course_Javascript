8b4bcefa9b19a96154c7e584d0e25707713bc941
Leonardo Versiani <henriversiani@gmail.com>
2024-10-14 15:10:57 -0300

    resolveu efeito colateral do botao

--- React/src/pages/Search/index.jsx
+++ React/src/pages/Search/index.jsx
@@ -4,54 +4,59 @@ import { fetchSearch } from "../../services/fetchApi";
 import Loading from "../../components/Loading";
 import './search.css'
 
-export function Search(){
+export function Search() {
 
-const params = useParams();
+    const params = useParams();
 
-const[data, setData] = useState([])
-const[value, setValue] = useState(10)
-const[buttonValue, setButtonValue] = useState(null)
+    const [data, setData] = useState([])
+    const [value, setValue] = useState(10)
+    const [buttonValue, setButtonValue] = useState(false)
 
-useEffect(()=>{
+    useEffect(() => {
 
-async function getResult() {
-    const result = await fetchSearch(params)
-    setData(result.results)
-}
+        async function getResult() {
+            const result = await fetchSearch(params)
+            setData(result.results)
+        }
+        
+        getResult()
+        
+    }, [])
 
-getResult()
+    useEffect(()=>{
+        console.log(data)
+        if(value >= data.length && data.length > 0){
+            setButtonValue(true)
+            console.log('estou aqui')
+        }
+    },[value])
 
-},[])
+    function handleClick() {
+        setValue((prev)=> prev + 10)
 
-function handleClick(){
-    setValue(value + 10)
-
-    console.log(data)
-    console.log(data.length)
-    console.log(value)  
+        /*    let contador = value
+            setValue((prev)=> prev + 10)
+            contador += 10
     
-    disableButton()
-}
-
-function disableButton(){
-    if(value + 10 >= data.length){
-        console.log('sumir com a bomba do botao')
-        setButtonValue(true) 
- }
-}
-
-
-    return(
+            if (contador >= data.length) {
+                setButtonValue(true)
+            } */
+    }
+    
+    
+    
+    return (
         <>
-        <h1>Search</h1>
-        {data ? data.map((element)=>(
-            <div className="card">
-            <img src={element.thumbnail} alt="" />
-            <h2 className="nome-produto" >{element.title}</h2>
-            <h3>R${element.price}</h3>
-          </div>
-        )).slice(0,value) : <Loading/> }
-            <button  className="more-button" hidden={buttonValue} onClick={handleClick}>More...</button>
+            <h1>Search</h1>
+            {data ? data.map((element) => (
+                <div className="card">
+                    <p>?</p>
+                    <img src={element.thumbnail} alt="" />
+                    <h2 className="nome-produto" >{element.title}</h2>
+                    <h3>R${element.price}</h3>
+                </div>
+            )).slice(0, value) : <Loading />}
+            <button className="more-button" hidden={buttonValue} onClick={handleClick}>More...</button>
         </>
     )
 } 
